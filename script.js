208abc327ccb068f3c4f5275b2e6c6405b4d710d
Jason <murangira99@gmail.com>
2024-02-02 11:19:40 +0200

    added function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
-  const hourHand = document.querySelector('.hour-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate() {
+    const now = new Date();

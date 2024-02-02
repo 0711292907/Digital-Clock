bae4f336d99024260c8fd5be07911cae4dbe467e
Jason <murangira99@gmail.com>
2024-02-02 11:20:30 +0200

    added second hand transformation

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
